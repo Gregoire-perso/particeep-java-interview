b7dcfd374f60d15d447cf665466fa65a051d2069
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2024-06-14 15:11:43 +0200

    feat(collection): complete collection file
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/main/java/collection/CollectionTest.java
+++ src/main/java/collection/CollectionTest.java
@@ -9,19 +9,24 @@ import java.util.List;
  */
 class CollectionTest {
 
+    /**
+     * operation : x -> ((x * 2) + 3) ^ 5
+     */
+    public static List<Double> compute1(List<Integer> input) {
+        return input.stream().map(integer -> Math.pow((integer * 2) + 3, 5)).toList();
+    }
+    
+    public static String capitalize(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 
-  /**
-   * operation : x -> ((x * 2) + 3) ^ 5
-   */
-  public static List<Double> compute1(List<Integer> input) {
-    return null;
-  }
-
-  /**
-   * operation : abc -> AbcAbc
-   */
-  public static List<String> compute2(List<String> input) {
-    return null;
-  }
-
+    /**
+     * operation : abc -> AbcAbc
+     */
+    public static List<String> compute2(List<String> input) {
+        return input.stream().map(string -> String.format("%s", capitalize(string)).repeat(2)).toList();
+    }
 }
