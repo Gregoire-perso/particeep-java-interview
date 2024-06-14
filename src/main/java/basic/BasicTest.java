49d88af197c6b3c3f410a6d140c1383dacf7454d
Grégoire Lefaure <gregoire.lefaure@epita.fr>
2024-06-14 15:11:29 +0200

    feat(basic): complete basic file
    Signed-off-by: Grégoire Lefaure <gregoire.lefaure@epita.fr>

--- src/main/java/basic/BasicTest.java
+++ src/main/java/basic/BasicTest.java
@@ -6,12 +6,48 @@ import io.vavr.control.Option;
  * For this basic test, you should not use any library. e.g. you should not use Math.pow for power method
  */
 public class BasicTest {
+    public static Integer multSafeOverflow(int left, int right) throws ArithmeticException {
+        int result = left * right;
+        if (result / right != left) {
+            throw new ArithmeticException();
+        }
+        
+        return result;
+    }
+  
+    public static Integer powerRec(Integer base, Integer exponent) throws ArithmeticException {
+        System.out.println(base + " " + exponent);
+        if (exponent == 0) {
+            return 1;
+        }
+        if (exponent == 1 || base == 0) {
+            return base;
+        }
+        
+        if (exponent % 2 == 0) {
+            return powerRec(multSafeOverflow(base, base), exponent / 2);
+        }
+        
+        return multSafeOverflow(powerRec(multSafeOverflow(base, base), exponent / 2), base);
+    }
 
-  /**
-   * return i ^ n for positive Integer, None otherwise
-   * alse return None in case of errors
-   */
-  public static Option<Integer> power(Integer i, Integer n) {
-    return Option.none();
-  }
+    /**
+     * return i ^ n for positive Integer, None otherwise
+     * return None in case of errors
+     */
+    public static Option<Integer> power(Integer i, Integer n) {
+        if (i < 0 || n < 0) {
+            return Option.none();
+        }
+        
+        Integer res;
+        
+        try {
+            res = powerRec(i, n);
+        } catch (ArithmeticException e) {
+            return Option.none();
+        }
+        
+        return Option.of(res);
+    }
 }
